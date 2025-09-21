1a0f0956256291a8a48aea84e65beb2a8c41f8b8
김홍범 <fkdldj48@naver.com>
2025-09-21 19:49:25 +0900

    init: frontend 초기 설정

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TalkToYou</h1>
+        <p>실시간 채팅 어플리케이션</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
